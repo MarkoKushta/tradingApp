d904ad1d629318477da8173a374449baf248f048
Marko-Kushta <markokushta39@gmail.com>
2023-08-30 03:44:03 +0200

    AddedMigrations

---
+++ Migrations/20230830013449_MigrationsUsersRemovedTokenKEKW.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tradingAppCS.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationsUsersRemovedTokenKEKW : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "tradingAppUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "tradingAppUsers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
